7bb4cc8bdb162fce1e7886cdf82dfe5eb2130d7b
ctianlong <chengtl0131@gmail.com>
2018-06-21 18:07:58 +0800

    test

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # CMDVAR="-Djava.security.egd=file:/dev/./urandom","java -agentlib:jdwp=transport=dt_socket,address=0:8000,server=y,suspend=n -jar","-javaagent:/opt/oss/servicemgr/ICAgent/pinpoint/pinpoint-bootstrap.jar -Dapm_application=tongji -Dapm_tier=auditserver"
-java $CMDVAR -jar ./userservice-0.0.1-SNAPSHOT.jar
+java $CMDVAR -jar ./user-service-0.0.1-SNAPSHOT.jar
